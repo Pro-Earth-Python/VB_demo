8a3baf6ba96a9516ed226d6df23a76a17bd85cab
MotheoMarutla <MotheoMarutla@LE-MOTHEO2025>
2025-02-06 11:27:19 +0200

    Add project files.

---
+++ VB_demo/Program.vb
@@ -0,0 +1,29 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        Dim name As String
+        Dim surname As String
+
+        Console.WriteLine("Please enter your name")
+
+        name = Console.ReadLine()
+        Console.WriteLine("Please enter your surname")
+        surname = Console.ReadLine()
+
+        Console.WriteLine("Hi my name is :" + name + " and my surname is :" + surname)
+
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+End Module
